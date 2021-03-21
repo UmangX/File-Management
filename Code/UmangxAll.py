7c29c00ba4c8ad2ebdf2dc7a3ef078ab7b0332a9
Umang Patel <69800542+UmangX@users.noreply.github.com>
2021-03-21 07:08:04 +0530

    cleanup
    added Support for zip doc and mp3

--- Code/UmangxAll.py
+++ Code/UmangxAll.py
@@ -1,9 +1,21 @@
 #Personal Project By Umangx.cf
 #for all the files Requires custom input
-from os import listdir 
+from os import listdir
 from os import chdir
 from os import system
 from getpass import getuser
+from os import path 
+
+
+user = getuser()
+
+def makefolders(nameoffolder):
+	if path.exists('c:/Users/'+user+'/'+nameoffolder) == False:
+		chdir('C:/Users/'+user)
+		system('mkdir '+ nameoffolder) 
+
+makefolders('Software')
+makefolders('Zips')
 
 
 #gets the username for windows
@@ -18,9 +30,12 @@ chdir(Download)
 
 
 #the main directory to move to //user-specific where the images should be stored 
-des = ('C:/Users/'+user+'/Pictures/') 
-des2 = ('D:\Softies')
-des3 =('D:\Books')
+picture = ('C:/Users/'+user+'/Pictures') 
+software = ('C:/Users/'+user+'/Software')
+docs =('C:/Users/'+user+'/Documents')
+vids = ('C:/Users/'+user+'/Videos')
+zips = ('C:/Users/'+user+'/Zips')
+Music = ('C:/Users/'+user+'/Music')
 
 
 #gets the list of files
@@ -28,23 +43,57 @@ files = listdir()
 
 
 def moveimages(i):
-	#checks for file extension 
+	#checks for file extension
 	if i[-3:len(i)] == "jpg":
 	   #print(i)
 	   #print("Image Found")
-	   system("move " + i +" " + des)
+	   system("move " + i +" " + picture)
 	if i[-3:len(i)] == "png":
 	   #print(i)
 	   #print("Image Found")
-	   system("move " + i +" " + des)
+	   system("move " + i +" " + picture)
 	if i[-3:len(i)] == "exe" or i[-3:len(i)] == "msi":
 	   #print(i)
 	   #print("Image Found")
-	   system("move " + i +" " + des2)
+	   system("move " + i +" " + software)
 	if i[-3:len(i)] == "pdf":
 	   #print(i)
 	   #print("Image Found")
-	   system("move " + i +" " + des3)
+	   system("move " + i +" " + docs)
+	if i[-3:len(i)] == "doc":
+	   #print(i)
+	   #print("Image Found") 
+	   system("move " + i +" " + docs)
+	if i[-3:len(i)] == "mp3":
+	   #print(i)
+	   #print("Image Found")
+	   system("move " + i +" " + Music)	   
+	if i[-3:len(i)] == "wav":
+	   #print(i)
+	   #print("Image Found")
+	   system("move " + i +" " + Music)		     
+	if i[-3:len(i)] == "zip":
+	   #print(i)
+	   #print("Image Found")
+	   system("move " + i +" " + zips)	   	
+	if i[-3:len(i)] == "iso":
+	   #print(i)
+	   #print("Image Found")
+	   system("move " + i +" " + zips)
+	if i[-3:len(i)] == "rar":
+	   #print(i)
+	   #print("Image Found")
+	   system("move " + i +" " + zips)	         
+	if i[-3:len(i)] == "mp4":
+	   #print(i)
+	   #print("Image Found")
+	   system("move " + i +" " + vids)
+	if i[-3:len(i)] == "mkv":
+	   #print(i)
+	   #print("Image Found")
+	   system("move " + i +" " + vids)	   
+	   
+	   
 
 
 
